0632a1c29d7bc84bbca56be0f92ab288f19beebb
ChandanBose666 <bosechandan21@gmail.com>
2022-03-15 03:44:05 +0530

    added top navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import styled from "styled-components";
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
   return (
     <Container>
         <a>
          <img src="/images/logo.svg" alt="logo" />   
-
         </a>
+
+        <Menu>
+           <a href="#">Model S</a>
+           <a href="#">Model Y</a>
+           <a href="#">Model 3</a>
+           <a href="#">Model X</a>
+        </Menu>
+        <RightMenu>
+           <a href="#">Shop</a>
+           <a href="#">Tesla Account</a>
+        </RightMenu>
+        <CustomMenu />
     </Container>
   )
 }
@@ -15,7 +27,40 @@ const Header = () => {
 export default Header;
 
 const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`
 
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 
+  a{
+    font-weight: 600 !important;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`
 
+const RightMenu = styled.div`
+display: flex;
+align-items: center;
+ a{
+    font-weight: 600 !important;
+    text-transform: uppercase;
+    margin-right: 10px;
+    flex-wrap: nowrap;
+  }
+`
+const CustomMenu = styled(MenuIcon)`
+cursor: pointer;
 `
